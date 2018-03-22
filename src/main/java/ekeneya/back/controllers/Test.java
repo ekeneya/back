964747646b6eaf46abebca002cab2005c678c322
zdiawara <zakaridiadiawara@gmail.com>
2018-03-22 13:52:03 +0100

    test

---
+++ src/main/java/ekeneya/back/controllers/Test.java
@@ -0,0 +1,38 @@
+package ekeneya.back.controllers;
+
+public class Test {
+
+	private String id;
+	private String nom;
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public Test(String id, String nom) {
+		super();
+		this.id = id;
+		this.nom = nom;
+	}
+	
+	
+}
--- src/main/java/ekeneya/back/controllers/TestController.java
+++ src/main/java/ekeneya/back/controllers/TestController.java
@@ -1,13 +1,22 @@
 package ekeneya.back.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 	
-	@RequestMapping("/")
-	public String hello(){
-		return "Hello";
+	@RequestMapping(value = "/all", method = RequestMethod.GET)
+	public ResponseEntity<List<Test>> getVilles() {
+		List<Test> lists = new ArrayList<>();
+		lists.add(new Test("2","nom 2"));
+		lists.add(new Test("1","nom 1"));
+		return ResponseEntity.ok(lists);
 	}
+	
 }

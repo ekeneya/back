482569e094edaf4ffc7ece7d2dd976c35f2ca9a1
zdiawara <zakaridiadiawara@gmail.com>
2018-03-22 13:42:20 +0100

    first

---
+++ src/main/java/ekeneya/back/controllers/TestController.java
@@ -0,0 +1,13 @@
+package ekeneya.back.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@RequestMapping("/")
+	public String hello(){
+		return "Hello";
+	}
+}
